daaf38f7367e0c602111c1fcef552f51c0c9484e
shaistaabidd <shaistaabid76@gmail.com>
2021-11-01 18:00:30 +0500

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_18_071022) do
+ActiveRecord::Schema.define(version: 2021_11_01_070452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,15 @@ ActiveRecord::Schema.define(version: 2021_10_18_071022) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "board_id"
     t.index ["board_id"], name: "index_lists_on_board_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "username"
+    t.string "task"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
