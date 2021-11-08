12148a3384ba328630a4e627c3c8bcc7865d781b
shaistaabidd <shaistaabid76@gmail.com>
2021-11-08 14:44:01 +0500

    create comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,10 +6,29 @@ class CommentsController < ApplicationController
     @comment=Comment.new
 
   end
+  
+  def create
+    @comment=Comment.new(comment_params)
+    @comment.user_id=current_user.id
+    if @comment.save
+      flash[:notice] = "Comment Added successfully......"
+      redirect_to("/comments")
+    else
+      render('new')
+    end
 
+  end
+  
   def show
   end
 
   def edit
   end
 end
+
+
+private
+
+  def comment_params
+    params.required(:comment).permit(:text)
+  end 
