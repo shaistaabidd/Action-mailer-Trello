76c5f71e34d275c321cdb5c0f42fed24e499e7fb
shaistaabidd <shaistaabid76@gmail.com>
2021-11-08 22:19:50 +0500

    add username in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_08_103722) do
+ActiveRecord::Schema.define(version: 2021_11_08_171415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2021_11_08_103722) do
     t.string "text", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.integer "user_name"
     t.integer "card_id"
   end
 
