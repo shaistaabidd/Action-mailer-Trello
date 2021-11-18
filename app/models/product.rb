d46e7a9753df899599c908d0eee2246709a01123
shaistaabidd <shaistaabid76@gmail.com>
2021-11-18 12:31:30 +0500

    add name of product and validate

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+  validates :name, 
+  :presence => true, :uniqueness => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_115555) do
+ActiveRecord::Schema.define(version: 2021_11_18_072550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,12 @@ ActiveRecord::Schema.define(version: 2021_11_15_115555) do
     t.integer "board_id"
     t.integer "user_id"
     t.index ["board_id"], name: "index_lists_on_board_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "roles", force: :cascade do |t|
