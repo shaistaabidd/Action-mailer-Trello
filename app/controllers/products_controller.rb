7cdb4e47165db500ca7100557853027f19f579a7
shaistaabidd <shaistaabid76@gmail.com>
2021-11-18 13:01:24 +0500

    create product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,23 @@
 class ProductsController < ApplicationController
-  
+  before_action :authenticate_user!
+  def new
+    @product=Product.new
+  end
+
+  def create
+    @product=Product.new(product_params)
+    if @product.save
+      flash[:notice]="Product Saved Successfully!!!"
+      redirect_to products_path
+    else
+      flash[:notice]="Try Again!!!"
+      render('new')
+    end
+  end
+
+  private
+    
+  def product_params
+    params.required(:product).permit(:name)
+  end 
 end
