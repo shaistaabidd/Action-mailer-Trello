cd8ab093bd10b12de1ff037b717011aed64d49d2
shaistaabidd <shaistaabid76@gmail.com>
2021-11-22 10:25:57 +0500

    create price

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,6 +1,26 @@
 class PricesController < ApplicationController
   def new
     @price = Price.new
+
+  end
+
+  def create
+    @price=Price.new(price_params)
+    @price.product_id=params[:id]
+    if @price.save
+      flash[:notice]="Price Save Successfully!!"
+      redirect_to boards_path
+    else
+      flash[:notice]="Invalid.. Try Again!!"
+      render('new')
+    end
   end
   
+
+  private
+
+  def price_params
+    params.required(:price).permit(:unit_amount,:recurring_interval)
+  end
+
 end
